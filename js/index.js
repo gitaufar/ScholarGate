094336d234b49d25cc3be0b465f7207a7b296b49
gitaufar <zhafiraufar123@gmail.com>
2024-11-25 16:18:22 +0700

    memberi contoh di index.js

--- js/index.js
+++ js/index.js
@@ -1,13 +1,16 @@
 var fetchapi = require('./fetchapi')
 
+//contoh ambil data listMahasiswa
 async function fetchMahasiswa() {
-    const mahasiswa = await fetchapi.getMahasiswa()
-    console.log(mahasiswa)
+    const listMahasiswa = await fetchapi.getMahasiswa()
+    return listMahasiswa
 }
 
+//contoh ambil data 1 mahsiswa
 async function fetchMahasiswaId(id = 'MHS-20231101') {
     const mahasiswa = await fetchapi.getMahasiswaId(id)
     console.log(mahasiswa)
 }
 
+fetchMahasiswa()
 fetchMahasiswaId()
