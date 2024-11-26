28895d8807979af0077ce3ddcfc7428287e9ee82
gitaufar <zhafiraufar123@gmail.com>
2024-11-26 17:59:45 +0700

    75% halaman mahasiswa

--- js/index.js
+++ js/index.js
@@ -1,16 +1,32 @@
-var fetchapi = require('./fetchapi')
+import { getMahasiswa, getMahasiswaId } from './fetchapi.js';
 
 //contoh ambil data listMahasiswa
 async function fetchMahasiswa() {
-    const listMahasiswa = await fetchapi.getMahasiswa()
-    return listMahasiswa
+    const listMahasiswa = await getMahasiswa()
+    listMahasiswa.forEach(element => {
+        const tableBody = document.querySelector("#dataTable tbody");
+        const newRow = document.createElement("tr"); 
+        newRow.innerHTML = `
+        <td>${element.id_mahasiswa}</td>
+        <td>${element.nama}</td>
+        <td>${element.nim}</td>
+        <td>${element.fakultas}</td>
+        <td>${element.jurusan}</td>
+        <td>${element.ipk}</td>
+        <td>${element.status_ekonomi}</td>
+        <td>${element.alamat_email}</td>
+        <td>${element.alamat}</td>
+        <td>${element.status_mahasiswa}</td>
+    `
+        tableBody.appendChild(newRow);
+    });
 }
 
 //contoh ambil data 1 mahsiswa
 async function fetchMahasiswaId(id = 'MHS-20231101') {
-    const mahasiswa = await fetchapi.getMahasiswaId(id)
+    const mahasiswa = await getMahasiswaId(id)
     console.log(mahasiswa)
 }
 
-fetchMahasiswa()
-fetchMahasiswaId()
+window.onload = fetchMahasiswa;
+console.log("index.js is connected and running!");
