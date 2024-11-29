87b6f702fc71fc2dec502e8264abb334acc897e2
rhesatsaqif23 <atstsaqif23@gmail.com>
2024-11-29 10:10:23 +0700

    BEasisswa

--- js/fetchapi.js
+++ js/fetchapi.js
@@ -1,47 +1,47 @@
-const baseUrl = 'http://localhost:8090/api'
+const baseUrl = "http://10.200.229.55:8090/api";
 
 async function getMahasiswa() {
   try {
-    const response = await fetch(baseUrl + '/mahasiswa');
-    console.log(baseUrl + '/mahasiswa')
-    console.log('Response Status:', response.status);
+    const response = await fetch(baseUrl + "/mahasiswa");
+    console.log(baseUrl + "/mahasiswa");
+    console.log("Response Status:", response.status);
     if (!response.ok) {
-      throw new Error('Network response was not ok');
+      throw new Error("Network response was not ok");
     }
     const data = await response.json();
     return data;
   } catch (error) {
-    console.log('Fetch error: ', error);
+    console.log("Fetch error: ", error);
     return null;
   }
 }
 
 async function getBeasiswa() {
   try {
-    const response = await fetch(baseUrl + '/beasiswa');
-    console.log('Response Status:', response.status);
+    const response = await fetch(baseUrl + "/beasiswa");
+    console.log("Response Status:", response.status);
     if (!response.ok) {
-      throw new Error('Network response was not ok');
+      throw new Error("Network response was not ok");
     }
     const data = await response.json();
     return data;
   } catch (error) {
-    console.log('Fetch error: ', error);
+    console.log("Fetch error: ", error);
     return null;
   }
 }
 
 async function getMahasiswaId(id) {
   try {
-    const response = await fetch(baseUrl + '/mahasiswa/' + id);
-    console.log('Response Status:', response.status);
+    const response = await fetch(baseUrl + "/mahasiswa/" + id);
+    console.log("Response Status:", response.status);
     if (!response.ok) {
-      throw new Error('Network response was not ok');
+      throw new Error("Network response was not ok");
     }
     const data = await response.json();
     return data;
   } catch (error) {
-    console.log('Fetch error: ', error);
+    console.log("Fetch error: ", error);
     return null;
   }
 }
